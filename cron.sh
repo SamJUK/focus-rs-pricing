7933f5561a5d7b179edf835cc66b78e4d780cdf5
Sam James <nintendosamhd@gmail.com>
2019-07-03 21:18:16 +0100

    Uncommented the CD line in cron script for server deploy

--- cron.sh
+++ cron.sh
@@ -1,4 +1,4 @@
-#cd /scripts/focus-rs-pricing && \
+cd /scripts/focus-rs-pricing && \
 make='FORD' model='FOCUS' aggregatedTrim='RS' postcode='CF54JS' years='2016,2017,2018' node index.js >> frs.txt && \
 make='FORD' model='FOCUS' aggregatedTrim='ST-3' postcode='CF54JS' years='2015,2016,2017,2018' fuel_type="Petrol" node index.js >> fstp.txt && \
 make='FORD' model='FOCUS' aggregatedTrim='ST-3' postcode='CF54JS' years='2015,2016,2017,2018' fuel_type="Diesel" node index.js >> fstd.txt
