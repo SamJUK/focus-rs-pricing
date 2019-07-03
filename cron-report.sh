5a7553ef10486ffb411c9b2bc7e96842eddd5a20
Sam James <nintendosamhd@gmail.com>
2019-07-03 20:31:19 +0100

    Added FST to crons

--- cron-report.sh
+++ cron-report.sh
@@ -1,1 +1,3 @@
-tail -n 34 /scripts/focus-rs-pricing/overview.txt | mail -s "Focus RS Pricing Report" -r cron@samdjames.uk samjuk1999@gmail.com
+tail -n 34 /scripts/focus-rs-pricing/frs.txt | mail -s "Focus RS Pricing Report" -r cron@samdjames.uk samjuk1999@gmail.com && \
+tail -n 49 /scripts/focus-rs-pricing/fstp.txt | mail -s "Focus ST3 Petrol Pricing Report" -r cron@samdjames.uk samjuk1999@gmail.com && \
+tail -n 49 /scripts/focus-rs-pricing/fstd.txt | mail -s "Focus ST3 Diesel Pricing Report" -r cron@samdjames.uk samjuk1999@gmail.com
--- cron.sh
+++ cron.sh
@@ -1,1 +1,4 @@
-cd /scripts/focus-rs-pricing && node index.js >> overview.txt
+#cd /scripts/focus-rs-pricing && \
+make='FORD' model='FOCUS' aggregatedTrim='RS' postcode='CF54JS' years='2016,2017,2018' node index.js >> frs.txt && \
+make='FORD' model='FOCUS' aggregatedTrim='ST-3' postcode='CF54JS' years='2015,2016,2017,2018' fuel_type="Petrol" node index.js >> fstp.txt && \
+make='FORD' model='FOCUS' aggregatedTrim='ST-3' postcode='CF54JS' years='2015,2016,2017,2018' fuel_type="Diesel" node index.js >> fstd.txt
